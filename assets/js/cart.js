997f85eb11eaf3f2fbe7a313f6a33b8d1fba76a8
al-sabana <118425884+al-sabana@users.noreply.github.com>
2023-03-15 16:30:46 +0530

    husky install

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -60,16 +60,71 @@ function addCart() {
     window.localStorage.setItem("items", JSON.stringify(cart_array));
   } else {
     let cart = window.localStorage.getItem("items");
-    let parseCart = JSON.parse(cart);
-    parseCart.push(items);
-    window.localStorage.setItem("items", JSON.stringify(parseCart));
+    let whole = JSON.parse(cart);
+    whole.push(items);
+    window.localStorage.setItem("items", JSON.stringify(whole));
   }
 }
 
+/* <div class="cart-box">
+<img src="../assets/iages "id="image" class="cart-img">
+<div class="detail-box">
+  <div id="product_name" class="product-title">caffeine shampoo</div>
+  <div class="price-box">
+    <div id="price" class="cart-price">Rs.678</div>
+    <div class="cart-amt">Rs.72</div>
+  </div>
+  <input type="number" value="1" class="cart-quantity">
+
+</div>
+<ion-icon name="trash" class="cart-remove"></ion-icon> */
+
+// </div>
 for (let i = 0; i < cart_array.length; i++) {
-  let detail = cart_array[i];
+  let detail = window.parse(localStorage.getItem("items"));
+  detail = cart_array[i];
+
   document.getElementById("product_name").innerHTML = detail.title;
   document.getElementById("price").innerHTML = detail.price;
   document.getElementById("image").src = detail.image;
-  cartArray.push(item);
 }
+
+let div_card = document.createElement("div");
+div_card.setAttribute("class", "cart-box");
+let card_img = document.createElement("img");
+card_img.setAttribute("src", "../assets/images/Caster-Hair-Oil-.jpg.webp");
+card_img.setAttribute("class", "cart-img");
+card_img.setAttribute("id", "image");
+div_card.append(card_img);
+let div_card1 = document.createElement("div");
+div_card1.setAttribute("class", "detail-box");
+div_card.append(div_card1);
+let div_card2 = document.createElement("div");
+div_card2.setAttribute("id", "product_name");
+div_card2.setAttribute("class", "product-title");
+div_card2.innerText = "Caffein shampoo";
+div_card1.append(div_card2);
+let price_div = document.createElement("div");
+price_div.setAttribute("class", "price-box");
+div_card1.append(price_div);
+let price_div1 = document.createElement("div");
+price_div1.setAttribute("id", "price");
+price_div1.setAttribute("class", "cart-price");
+price_div1.innerText = "Rs,124";
+price_div.append(price_div1);
+let price_div2 = document.createElement("div");
+price_div2.setAttribute("class", "cart-amt");
+price_div2.innerText = "Rs,124";
+price_div.append(price_div2);
+let input = document.createElement("input");
+input.setAttribute("value", "1");
+input.setAttribute("type", "number");
+input.setAttribute("class", "cart-quantity");
+div_card1.append(input);
+let ion = document.createElement("ion-icon");
+ion.setAttribute("class", "cart-remove");
+ion.setAttribute("name", "trash");
+div_card.append(ion);
+let cart_content = document.querySelector(".cart-content");
+cart_content.prepend(div_card);
+console.log(cart_content);
