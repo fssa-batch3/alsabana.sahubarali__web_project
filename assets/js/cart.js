24b726774b49bc89db4ad84f9ab47dfb2e6ca73c
al-sabana <118425884+al-sabana@users.noreply.github.com>
2023-03-20 14:28:59 +0530

    build:cart module

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,3 +1,5 @@
+// const { json } = require("stream/consumers");
+
 const btnCart = document.querySelector(".count");
 const cart = document.querySelector(".cart");
 const btnClose = document.querySelector("#cart-close");
@@ -15,7 +17,7 @@ function loadProduct() {
   loadContent();
 }
 function loadContent() {
-  //Remove food items from cart
+  //Remove food cart_array from cart
   let btnRemove = document.querySelectorAll(".cart-remove");
   btnRemove.forEach((btn) => {
     btn.addEventListener("click", removeItem);
@@ -28,15 +30,46 @@ function loadContent() {
 }
 //product cart
 let cartBtns = document.querySelectorAll(".add_Cart");
+
 cartBtns.forEach((btn) => {
   btn.addEventListener("click", addCart);
 });
-//remove item
+
+// remove item
+// function removeItem() {
+//   if (confirm("Are you sure to remove")) {
+//     console.log(this.parentElement);
+//     this.parentElement.remove();
+//     window.localStorage.setItem("items", JSON.stringify(this.parentElement));
+//   }
+// }
+
 function removeItem() {
-  if (confirm("Are you sure to remove")) {
-    this.parentElement.remove();
+  let product = this.parentElement;
+  let title = product.querySelector(".product-title").innerHTML;
+  let price = product.querySelector(".cart-price").innerHTML;
+  let image = product.querySelector(".cart-img").src;
+
+  let items = {
+    title: title,
+    price: price,
+    image: image,
+    quantity: 1,
+  };
+  let checkQty = JSON.parse(localStorage.getItem("items"));
+  for (var i = 0; i < checkQty?.length; i++) {
+    var key = checkQty[i];
+    if (key.title == items.title) {
+      if (key) {
+        console.log("hi", delete key);
+        console.log(checkQty);
+      }
+      localStorage.setItem("items", JSON.stringify(checkQty));
+      break;
+    }
   }
 }
+
 //change quantity
 function changeQty() {
   if (isNaN(this.value) || this.value < 1) {
@@ -44,7 +77,7 @@ function changeQty() {
   }
 }
 // add cart
-let cart_array = [];
+
 function addCart() {
   let product = this.parentElement;
   let title = product.querySelector(".product-name").innerHTML;
@@ -55,76 +88,95 @@ function addCart() {
     title: title,
     price: price,
     image: image,
+    quantity: 1,
   };
-  if (window.localStorage.getItem("items") === null) {
-    window.localStorage.setItem("items", JSON.stringify(cart_array));
-  } else {
-    let cart = window.localStorage.getItem("items");
-    let whole = JSON.parse(cart);
-    whole.push(items);
-    window.localStorage.setItem("items", JSON.stringify(whole));
+  let checkQty = JSON.parse(localStorage.getItem("items"));
+
+  for (var i = 0; i < checkQty?.length; i++) {
+    var key = checkQty[i];
+    var haveTitle = true;
+    if (key.title == items.title) {
+      if (key) {
+        key.quantity += items.quantity;
+      }
+      localStorage.setItem("items", JSON.stringify(checkQty));
+      haveTitle = false;
+      break;
+    }
   }
+
+  if (checkQty == null) {
+    console.log("hello");
+    window.localStorage.setItem("items", JSON.stringify([items]));
+  } else {
+    if (haveTitle) {
+      let cart = window.localStorage.getItem("items");
+      let whole_cart = JSON.parse(cart);
+      whole_cart.push(items);
+      window.localStorage.setItem("items", JSON.stringify(whole_cart));
+    } else {
+      alert("exist");
+    }
+  }
+  location.reload();
 }
 
-/* <div class="cart-box">
-<img src="../assets/iages "id="image" class="cart-img">
-<div class="detail-box">
-  <div id="product_name" class="product-title">caffeine shampoo</div>
-  <div class="price-box">
-    <div id="price" class="cart-price">Rs.678</div>
-    <div class="cart-amt">Rs.72</div>
-  </div>
-  <input type="number" value="1" class="cart-quantity">
+let getCartItems = JSON.parse(localStorage.getItem("items"));
 
-</div>
-<ion-icon name="trash" class="cart-remove"></ion-icon> */
+for (let i = 0; i < getCartItems?.length; i++) {
+  let div_card = document.createElement("div");
+  div_card.setAttribute("class", "cart-box");
+  let card_img = document.createElement("img");
+  card_img.setAttribute("src", getCartItems[i].image);
+  card_img.setAttribute("class", "cart-img");
+  card_img.setAttribute("id", "image");
+  div_card.append(card_img);
+  let div_card1 = document.createElement("div");
+  div_card1.setAttribute("class", "detail-box");
+  div_card.append(div_card1);
+  let div_card2 = document.createElement("div");
+  div_card2.setAttribute("id", "product_name");
+  div_card2.setAttribute("class", "product-title");
+  div_card2.innerText = getCartItems[i].title;
+  div_card1.append(div_card2);
+  let price_div = document.createElement("div");
+  price_div.setAttribute("class", "price-box");
+  div_card1.append(price_div);
+  let price_div1 = document.createElement("div");
+  price_div1.setAttribute("id", "price");
+  price_div1.setAttribute("class", "cart-price");
+  price_div1.innerText = getCartItems[i].price;
+  price_div.append(price_div1);
+  let price_div2 = document.createElement("div");
+  price_div2.setAttribute("class", "cart-amt");
+  price_div2.innerText = getCartItems[i].price;
+  price_div.append(price_div2);
+  let input = document.createElement("input");
+  input.setAttribute("value", getCartItems[i].quantity);
+  input.setAttribute("type", "number");
+  input.setAttribute("class", "cart-quantity");
+  div_card1.append(input);
 
-// </div>
-for (let i = 0; i < cart_array.length; i++) {
-  let detail = window.parse(localStorage.getItem("items"));
-  detail = cart_array[i];
+  let ion = document.createElement("ion-icon");
+  ion.setAttribute("class", "cart-remove");
+  ion.setAttribute("id", "delete");
+  ion.setAttribute("name", "trash");
+  // ion.setAttribute("onclick", "del(" + getCartItems[i].cart_id + ")");
+  div_card.append(ion);
 
-  document.getElementById("product_name").innerHTML = detail.title;
-  document.getElementById("price").innerHTML = detail.price;
-  document.getElementById("image").src = detail.image;
+  let cart_content = document.querySelector(".cart-content");
+  cart_content.prepend(div_card);
 }
 
-let div_card = document.createElement("div");
-div_card.setAttribute("class", "cart-box");
-let card_img = document.createElement("img");
-card_img.setAttribute("src", "../assets/images/Caster-Hair-Oil-.jpg.webp");
-card_img.setAttribute("class", "cart-img");
-card_img.setAttribute("id", "image");
-div_card.append(card_img);
-let div_card1 = document.createElement("div");
-div_card1.setAttribute("class", "detail-box");
-div_card.append(div_card1);
-let div_card2 = document.createElement("div");
-div_card2.setAttribute("id", "product_name");
-div_card2.setAttribute("class", "product-title");
-div_card2.innerText = "Caffein shampoo";
-div_card1.append(div_card2);
-let price_div = document.createElement("div");
-price_div.setAttribute("class", "price-box");
-div_card1.append(price_div);
-let price_div1 = document.createElement("div");
-price_div1.setAttribute("id", "price");
-price_div1.setAttribute("class", "cart-price");
-price_div1.innerText = "Rs,124";
-price_div.append(price_div1);
-let price_div2 = document.createElement("div");
-price_div2.setAttribute("class", "cart-amt");
-price_div2.innerText = "Rs,124";
-price_div.append(price_div2);
-let input = document.createElement("input");
-input.setAttribute("value", "1");
-input.setAttribute("type", "number");
-input.setAttribute("class", "cart-quantity");
-div_card1.append(input);
-let ion = document.createElement("ion-icon");
-ion.setAttribute("class", "cart-remove");
-ion.setAttribute("name", "trash");
-div_card.append(ion);
-let cart_content = document.querySelector(".cart-content");
-cart_content.prepend(div_card);
-console.log(cart_content);
+//delete
+// function del(id) {
+//   const arr = JSON.parse(localStorage.getItem("items"));
+//   let delobj = arr.find(function (event) {
+//     if (id === e.cart_id) {
+//       return true;
+//     }
+//   });
+//   let index = arr.indexOf(delobj);
+//   arr.splice(index, 1);
+//   alert("removed sucessfully ");
+// }
