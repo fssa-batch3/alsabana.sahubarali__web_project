acd4ad0cbfd524b33725a4a57132dccb1c217abf
al-sabana <118425884+al-sabana@users.noreply.github.com>
2023-04-25 12:29:25 +0530

    update

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -1,3 +1,23 @@
+const product_id = window.location.search;
+const product_params = new URLSearchParams(product_id);
+const get_details = product_params.get("id");
+const products = JSON.parse(localStorage.getItem("proObject"));
+let pro_detail = products.find(function (event) {
+  let id = event["id"];
+  if (get_details == id) {
+    return true;
+  }
+});
+
+let images = document.getElementById("imgs");
+let product_name = document.getElementById("pro_name");
+let product_price = document.getElementById("pro_cost");
+let description = document.getElementById("description");
+product_name.innerText = products["productName"];
+product_price.innerText = products["cost"];
+description.innerText = products["product_detail"];
+images.setAttribute("src", products["src"]);
+
 const allstar = document.querySelectorAll(".fa-regular");
 const showRating = document.getElementById("showRating");
 allstar.forEach((star, index) => {
@@ -22,5 +42,4 @@ let input = document.getElementById("input");
 input.addEventListener("submit", function (event) {
   event.preventDefault();
   let review = document.getElementById("review").value;
-  console.log(review);
 });
